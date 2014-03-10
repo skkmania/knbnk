878cd91b35d4045bc71ab12f6c7677821fcfa3aa
skkmania <skkmania@gmail.com>
2014-03-10 22:21:16 +0900

    collect bounding boxes

--- classes/knpage.py
+++ classes/knpage.py
@@ -28,7 +28,7 @@ from operator import itemgetter, attrgetter
 
 class KnPage:
 
-  def __init__(self, fname):
+  def __init__(self, fname, params=None):
     if os.path.exists(fname):
       self.img = cv2.imread(fname)
       self.original_file_name = fname
@@ -37,19 +37,34 @@ class KnPage:
       else:
         self.height, self.width, self.depth = self.img.shape
         self.centroids = []
+        self.boxes = []
+        if params:
+          if os.path.exists(params):
+            self.read_params(params)
+          else:
+            raise
     else:
       raise
      
-
+  def read_params(self, params):
+    f = open(params)
+    lines = f.readlines()
+    self.parameters = json.loads(''.join(lines))
+    self.outfilename = self.parameters['outfilename']
 
   def divide(self):
     self.left = None
     self.right = None
 
-  def write(self, outfilename, om=None):
+  def write(self, outfilename=None, om=None):
     if om == None:
         om = self.img
+    #if hasattr(self, 'outfilename'):
+    #    outfilename = self.outfilename
+    #if outfilename:
     cv2.imwrite(outfilename, om)
+    #else:
+    #  raise
 
   def update(self, v):
     self.val = v
@@ -66,10 +81,14 @@ class KnPage:
     basename = os.path.basename(self.original_file_name) 
     if fix == 'data':
       name, ext = os.path.splitext(basename) 
+      if hasattr(self, 'outfilename'):
+        name = self.outfilename
       name = name + '_data'
       ext = '.txt'
     else:
       name, ext = os.path.splitext(basename) 
+      if hasattr(self, 'outfilename'):
+        name = self.outfilename
       name = name + fix
 
     if outdir == None:
@@ -82,15 +101,32 @@ class KnPage:
     if not hasattr(self, 'contours'):
       self.getContours()
 
+    if hasattr(self, 'parameters'):
+      if self.parameters.has_key('boundingRect'):
+          box_min, box_max = self.parameters['boundingRect']
+
     for cnt in self.contours:
-      x,y,w,h = cv2.boundingRect(cnt)
+      box = cv2.boundingRect(cnt)
+      self.boxes.append(box)
+      x,y,w,h = box
       if (int(w) in range(box_min, box_max)) or (int(h) in range(box_min, box_max)):
         self.centroids.append((x+w/2, y+h/2))
 
   # contourの配列を返す
   def getContours(self, thresh_low=50, thresh_high=255):
     self.gray = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
-    ret,self.thresh = cv2.threshold(self.gray, thresh_low, thresh_high, 0)
+    if hasattr(self, 'parameters'):
+      if self.parameters.has_key('threshold'):
+        thresh_low, thresh_high, typeval = self.parameters['threshold']
+        ret,self.thresh = cv2.threshold(self.gray, thresh_low, thresh_high, typeval)
+      elif self.parameters.has_key('canny'):
+          minval, maxval = self.parameters['canny']
+          self.thresh = cv2.Canny(self.gray,minval, maxval)
+      elif self.parameters.has_key('adaptive'):
+          self.thresh = cv2.adaptiveThreshold(self.gray,self.parameters['adaptive'])
+    else:
+      ret,self.thresh = cv2.threshold(self.gray, thresh_low, thresh_high, 0)
+
     self.contours, self.hierarchy = cv2.findContours(self.thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
   def writeContour(self):
@@ -158,3 +194,98 @@ class KnPage:
       cv2.circle(om, (cx,cy), 2, [0,0,255])
     outfilename = self.mkFilename('_orig_w_cont_and_rect', outdir)
     self.write(outfilename,self.orig_w_cont_and_rect)
+
+  def write_all(self, outdir):
+      self.write_data_file(outdir)
+      self.write_binarized_file(outdir)
+      self.write_contours_bounding_rect_to_file(outdir)
+      self.write_original_with_contour_file(outdir)
+      self.write_original_with_contour_and_rect_file(outdir)
+      self.write_original_with_collected_boxes_to_file(outdir)
+
+  def intersect(self, box1, box2):
+      #""" box1 と box2 が交わるか接するならtrueを返す。
+      ax1,ay1,w1,h1 = box1
+      ax2 = ax1 + w1
+      ay2 = ay1 + h1
+      bx1,by1,w2,h2 = box2
+      bx2 = bx1 + w2
+      by2 = by1 + h2
+
+      if (bx1 in range(ax1 - 1, ax2 + 2)) and (by1 in range(ay1 - 1, ay2 + 2)):
+          return True
+      if (bx2 in range(ax1 - 1, ax2 + 2)) and (by2 in range(ay1 - 1, ay2 + 2)):
+          return True
+      if (bx2 in range(ax1 - 1, ax2 + 2)) and (by1 in range(ay1 - 1, ay2 + 2)):
+          return True
+      if (bx1 in range(ax1 - 1, ax2 + 2)) and (by2 in range(ay1 - 1, ay2 + 2)):
+          return True
+      if (ax1 in range(bx1 - 1, bx2 + 2)) and (ay1 in range(by1 - 1, by2 + 2)):
+          return True
+      if (ax2 in range(bx1 - 1, bx2 + 2)) and (ay2 in range(by1 - 1, by2 + 2)):
+          return True
+      if (ax2 in range(bx1 - 1, bx2 + 2)) and (ay1 in range(by1 - 1, by2 + 2)):
+          return True
+      if (ax1 in range(bx1 - 1, bx2 + 2)) and (ay2 in range(by1 - 1, by2 + 2)):
+          return True
+
+      return False
+
+  def get_boundingBox(self, boxes):
+      # (x,y,w,h) -> (x,y,x+w,y+h)
+      target = [(x,y,x+w,y+h) for (x,y,w,h) in boxes]
+      x1,y1,d1,d2 = map(min, zip(*target))
+      d1,d2,x2,y2 = map(max, zip(*target))
+      # (x,y,x+w,y+h) -> (x,y,x,y)
+      return (x1,y1,x2-x1,y2-y1)
+  
+  def collect_boxes(self):
+      #""" bounding boxを包含するboxに統合し、文字を囲むboxの取得を試みる"""
+      
+    if len(self.boxes) == 0:
+        self.getCentroids()
+        
+    # w, h どちらかが200以上のboxは排除
+    self.boxes = [x for x in self.boxes if (x[2] < 200) and (x[3] < 200)]
+
+    self.collected_boxes = []
+    temp_boxes = []
+
+    while len(self.boxes) > 0:
+      abox = self.boxes.pop()
+      temp_boxes = [x for x in self.boxes if self.intersect(abox, x)]  
+      self.boxes = list(set(self.boxes) - set(temp_boxes))
+      temp_boxes.append(abox)
+      self.collected_boxes.append(self.get_boundingBox(temp_boxes))
+
+
+  def write_collected_boxes_to_file(self, outdir=None):
+    if not hasattr(self, 'collected_boxes'):
+      self.collect_boxes()
+      self.boxes = self.collected_boxes
+      self.collect_boxes()
+      self.boxes = self.collected_boxes
+      self.collect_boxes()
+
+    om = np.zeros(self.img.shape,np.uint8)
+    for box in self.collected_boxes:
+      x,y,w,h = box
+      cv2.rectangle(om, (x, y), (x+w, y+h), [0,0,255])
+    self.write(self.mkFilename('_collected_box', outdir), om)
+
+  def write_original_with_collected_boxes_to_file(self, outdir=None):
+    if not hasattr(self, 'collected_boxes'):
+      self.collect_boxes()
+      self.boxes = self.collected_boxes
+      self.collect_boxes()
+      self.boxes = self.collected_boxes
+      self.collect_boxes()
+
+    self.orig_w_collected = self.img.copy()
+    om = self.orig_w_collected
+    for box in self.collected_boxes:
+      x,y,w,h = box
+      #cv2.rectangle(om, (x, y), (w, h), [0,0,255])
+      cv2.rectangle(om, (x, y), (x+w, y+h), [0,0,255])
+    self.write(self.mkFilename('_orig_w_collected_box', outdir), om)
+
--- classes/test/test_knpage.py
+++ classes/test/test_knpage.py
@@ -22,12 +22,35 @@ class TestKnPage(unittest.TestCase):
     self.assertEqual(kn.depth, 3)
     return kn
 
-  @pytest.fixture
-  def test_separate(self, newimg):
+  def test_new_with_params(self):
+    fname = '/home/skkmania/workspace/pysrc/knpage/data/twletters.jpg'
+    params = self.DATA_DIR + '/twletters_01.json'
+    kn = KnPage(fname, params)
+    assert kn.img != None
+    assert kn.img.shape != (100,100,3)
+    #self.assertTupleEqual(kn.img.shape, (100,100,3))
+    self.assertTupleEqual(kn.img.shape, (558,669,3))
+    self.assertEqual(kn.height, 558)
+    self.assertEqual(kn.width, 669)
+    self.assertEqual(kn.depth, 3)
+    return kn
+
+  def test_read_params(self):
+    fname = '/home/skkmania/workspace/pysrc/knpage/data/twletters.jpg'
+    kn = KnPage(fname)
+    params = self.DATA_DIR + '/twletters_01.json'
+    kn.read_params(params)
+    assert not kn.parameters == None
+
+
+  def test_separate(self):
+    fname = '/home/skkmania/workspace/pysrc/knpage/data/twletters.jpg'
+    kn = KnPage(fname)
     expected = "Hello, World!"
-    arr = newimg.getContours()[0]
+    kn.getContours()
+    arr = kn.contours
     x = range(1,len(arr))
-    actual = newimg.separate(arr, x)
+    actual = kn.separate(arr, x)
 
     assert_that(actual, equal_to(expected))
     # PyHamcrest を使わずに assert する場合は以下のように書く
@@ -104,3 +127,57 @@ class TestKnPage(unittest.TestCase):
       fname = '/home/skkmania/workspace/pysrc/knpage/data/twletters.jpg'
       kn = KnPage(fname)
       kn.write_original_with_contour_and_rect_file(self.DATA_DIR)
+
+  def test_write_all_with_params(self):
+      fname = '/home/skkmania/workspace/pysrc/knpage/data/twletters.jpg'
+      params = self.DATA_DIR + '/twletters_01.json'
+      kn = KnPage(fname, params)
+      kn.write_data_file(self.DATA_DIR)
+      kn.write_binarized_file(self.DATA_DIR)
+      kn.write_contours_bounding_rect_to_file(self.DATA_DIR)
+      kn.write_original_with_contour_file(self.DATA_DIR)
+      kn.write_original_with_contour_and_rect_file(self.DATA_DIR)
+
+  def test_write_all(self):
+      fname = '/home/skkmania/workspace/pysrc/knpage/data/twletters.jpg'
+      for i in range(2,9):
+        params = self.DATA_DIR + '/twletters_0' + str(i) + '.json'
+        kn = KnPage(fname, params)
+        kn.write_all(self.DATA_DIR)
+
+  def test_intersect(self):
+      box1 = (20, 30, 10, 10)
+      box2 = (25, 35, 15, 15)
+      box3 = (35, 45, 10, 10)
+      box4 = (35, 20, 20, 20)
+      box5 = (10, 45, 20, 20)
+      fname = '/home/skkmania/workspace/pysrc/knpage/data/twletters.jpg'
+      kn = KnPage(fname)
+      assert kn.intersect(box1, box2)
+      assert not kn.intersect(box1, box3)
+      assert not kn.intersect(box1, box4)
+      assert not kn.intersect(box1, box5)
+      assert kn.intersect(box2, box3)
+      assert kn.intersect(box2, box4)
+      assert kn.intersect(box2, box5)
+      assert not kn.intersect(box3, box4)
+      assert not kn.intersect(box3, box5)
+      assert not kn.intersect(box4, box5)
+
+  def test_get_boundingBox(self):
+      box1 = (20, 30, 10, 10)
+      box2 = (25, 35, 15, 15)
+      box3 = (35, 45, 10, 10)
+      box4 = (35, 20, 20, 20)
+      box5 = (10, 45, 20, 20)
+      fname = '/home/skkmania/workspace/pysrc/knpage/data/twletters.jpg'
+      kn = KnPage(fname)
+      outer_box = kn.get_boundingBox([box1, box2,box3])
+      assert outer_box == (20,30,25,25)
+      outer_box = kn.get_boundingBox([box1, box2,box3,box4,box5])
+      assert outer_box == (10,20,45,45)
+
+  def test_write_original_with_contour_and_rect_file(self):
+      fname = '/home/skkmania/workspace/pysrc/knpage/data/twletters.jpg'
+      kn = KnPage(fname)
+      kn.write_original_with_collected_boxes_to_file(self.DATA_DIR)
